b08cae6fe0b9d54f6a13690dbcd1e14d37a32a97
root <root@li687-97.members.linode.com>
2020-05-01 13:48:55 +0000

    + add socket opt

--- wordpress_scripts/mysql/mysql-8.0.19-shutdown.sh
+++ wordpress_scripts/mysql/mysql-8.0.19-shutdown.sh
@@ -1,6 +1,7 @@
 mysql_install_dir=/lgxzj-install/mysql
 mysql_user=test-mysql
 mysql_root_password=18712726983c++
+mysql_sock_loc=${mysql_install_dir}/data/mysql.sock
 
 cd ${mysql_install_dir}
-./bin/mysqladmin shutdown -u root -p${mysql_root_password}
+./bin/mysqladmin shutdown --socket=${mysql_sock_loc} -u root -p${mysql_root_password}
