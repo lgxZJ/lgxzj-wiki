faa4e9a14c912ba0ae93e2574e4943bca119533a
lgxZJ <lgxZJ@outlook.com>
2020-05-01 19:15:01 +0800

    fix typo, user

--- wordpress_scripts/mysql/mysql-8.0.19-start.sh
+++ wordpress_scripts/mysql/mysql-8.0.19-start.sh
@@ -1,6 +1,6 @@
 mysql_install_dir=/lgxzj-install/mysql
 mysql_conf_path=${mysql_install_dir}/etc/my.cnf
-mysql_user=test-mysql
+mysql_user=lgxzj-mysql
 
 cd ${mysql_install_dir}
-./bin/mysqld_safe --defaults-file=${mysql_config_path}
+./bin/mysqld_safe --defaults-file=${mysql_conf_path}
