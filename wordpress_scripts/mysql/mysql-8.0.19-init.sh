8c3496ef2893ebeb357692f7c4c1c0ded1e9e9fc
root <root@li687-97.members.linode.com>
2020-05-01 13:47:58 +0000

    finish init script

--- wordpress_scripts/mysql/mysql-8.0.19-init.sh
+++ wordpress_scripts/mysql/mysql-8.0.19-init.sh
@@ -1,17 +1,32 @@
 script_dir=`pwd`
 mysql_install_dir=/lgxzj-install/mysql
+mysql_user=lgxzj-mysql
+mysql_group=lgxzj-mysql
 
 #############################
 # Prepare Option Files
 #############################
+rm -rf ${mysql_install_dir}/data
+mkdir ${mysql_install_dir}/data
+chown -R ${mysql_user} ${mysql_install_dir}/data
+chmod 777 -R ${mysql_install_dir}/data
+
 rm -rf ${mysql_install_dir}/etc
 mkdir ${mysql_install_dir}/etc
+chown -R ${mysql_user} ${mysql_install_dir}/etc
+chmod 777 -R ${mysql_install_dir}/etc
 
 rm -rf ${mysql_install_dir}/logs
 mkdir ${mysql_install_dir}/logs
+chown -R ${mysql_user} ${mysql_install_dir}/logs
+chmod 777 -R ${mysql_install_dir}/logs
+touch ${mysql_install_dir}/logs/mysql.error
+chmod o+rw ${mysql_install_dir}/logs/mysql.error
 
 rm -rf ${mysql_install_dir}/tmp
 mkdir ${mysql_install_dir}/tmp
+chown -R ${mysql_user} ${mysql_install_dir}/tmp
+chmod 777 -R ${mysql_install_dir}/tmp
 
 cp ./my.cnf ${mysql_install_dir}/etc
 
@@ -23,9 +38,6 @@ cd ${mysql_install_dir}
 ##############################
 # Create user and group
 ##############################
-mysql_user=lgxzj-mysql
-mysql_group=lgxzj-mysql
-
 groupadd ${mysql_group}
 # create a non login permission user
 useradd -r -g ${mysql_group} -s /bin/false ${mysql_user}
@@ -45,6 +57,8 @@ mysql_conf_path=${mysql_install_dir}/etc/my.cnf
 ##############################
 cd ${script_dir}
 ./mysql-8.0.19-start.sh &
+./mysql-8.0.19-wait-pid.sh
 
+mysql_sock_loc=${mysql_install_dir}/data/mysql.sock
 cd ${mysql_install_dir}
-./bin/mysql -u root --skip-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '18712726983c++'"
+./bin/mysql --socket=${mysql_sock_loc} -u root --skip-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '18712726983c++'"
