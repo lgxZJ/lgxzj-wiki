34e56fb09e066d819d0dad42e5243b5bb3d3c7b1
lgxZJ <lgxZJ@outlook.com>
2020-05-01 19:14:12 +0800

    fix var name typo, lift dir var to the beginning

--- wordpress_scripts/mysql/mysql-8.0.19-init.sh
+++ wordpress_scripts/mysql/mysql-8.0.19-init.sh
@@ -1,3 +1,6 @@
+script_dir=`pwd`
+mysql_install_dir=/lgxzj-install/mysql
+
 #############################
 # Prepare Option Files
 #############################
@@ -15,7 +18,6 @@ cp ./my.cnf ${mysql_install_dir}/etc
 ##############################
 # Enter the install dir
 ##############################
-mysql_install_dir=/lgxzj-install/mysql
 cd ${mysql_install_dir}
 
 ##############################
@@ -36,9 +38,13 @@ mysql_conf_path=${mysql_install_dir}/etc/my.cnf
 
 # this will cause mysqld to initialize the data directory
 # with the given user as the default owner
-./bin/mysqld --defaults-file=${mysql_config_path} --initialize-insecure
+./bin/mysqld --defaults-file=${mysql_conf_path} --initialize-insecure
 
 ##############################
 # Change Local Root Password
 ##############################
+cd ${script_dir}
+./mysql-8.0.19-start.sh &
+
+cd ${mysql_install_dir}
 ./bin/mysql -u root --skip-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '18712726983c++'"
