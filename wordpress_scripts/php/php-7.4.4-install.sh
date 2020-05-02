2821842f7007f34076e4a8201063e45ef56bb097
root <root@li687-97.members.linode.com>
2020-05-02 01:37:22 +0000

    + add make install

--- wordpress_scripts/php/php-7.4.4-install.sh
+++ wordpress_scripts/php/php-7.4.4-install.sh
@@ -9,5 +9,6 @@ cd lgxzj-php
 wget https://www.php.net/distributions/php-7.4.4.tar.gz
 tar -zxvf php-7.4.4.tar.gz
 cd php-7.4.4
-./configure --prefix=/lgxzj-install/php --with-fpm-user=lgxzj-php --with-fpm-group=lgxzj-php
+./configure --prefix=/lgxzj-install/php --enable-fpm --with-fpm-user=lgxzj-php --with-fpm-group=lgxzj-php
 make
+make install
