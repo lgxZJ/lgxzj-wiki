8fb3d7f10f8e3c090d879f6b1f984b1b225b9700
root <root@li687-97.members.linode.com>
2020-05-10 06:17:35 +0000

    + change conf dir

--- wordpress_scripts/wordpress/wordpress-deploy.sh
+++ wordpress_scripts/wordpress/wordpress-deploy.sh
@@ -1,7 +1,7 @@
 cd lgxzj-wordpress
 cd wordpress
 
-blog_dir=/lgxzj-install/nginx/html/blog
+blog_dir=/lgxzj-install/nginx/html
 rm -rf ${blog_dir}
 mkdir -p ${blog_dir}
 cp -R ./* ${blog_dir}
--- wordpress_scripts/wordpress/wordpress-install.sh
+++ wordpress_scripts/wordpress/wordpress-install.sh
@@ -6,6 +6,6 @@ wget https://wordpress.org/latest.tar.gz
 tar -xzvf latest.tar.gz
 cp ../wp-config.php wordpress/wp-config.php
 
-blog_dir=/lgxzj-install/nginx/html/blog
+blog_dir=/lgxzj-install/nginx/html
 rm -rf ${blog_dir}
 mkdir ${blog_dir}
