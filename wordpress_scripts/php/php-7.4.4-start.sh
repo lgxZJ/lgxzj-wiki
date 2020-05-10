6ba1cfdb26e0b5b0cece3ea63b0b0228412b47b9
root <root@li687-97.members.linode.com>
2020-05-10 13:37:11 +0000

    + add php.ini

--- wordpress_scripts/php/php-7.4.4-start.sh
+++ wordpress_scripts/php/php-7.4.4-start.sh
@@ -3,5 +3,6 @@ php_install_dir=/lgxzj-install/php
 mv ${php_install_dir}/etc/php-fpm.conf.default ${php_install_dir}/etc/php-fpm.conf
 rm -f ${php_install_dir}/etc/php-fpm.d/www.conf.default
 cp ./wordpress.conf ${php_install_dir}/etc/php-fpm.d/
+cp ./php.ini ${php_install_dir}/etc
 
-${php_install_dir}/sbin/php-fpm 
+${php_install_dir}/sbin/php-fpm  --fpm-config ${php_install_dir}/etc/php-fpm.conf -c ${php_install_dir}/etc/php.ini
