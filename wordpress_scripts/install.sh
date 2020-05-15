98bf4b8bbd131f51f5a73697065efd5a9aed64c5
root <root@li687-97.members.linode.com>
2020-05-15 14:08:48 +0000

    + add all-in-one script

---
+++ wordpress_scripts/install.sh
@@ -0,0 +1,22 @@
+./inf-install.sh
+
+cd mysql
+./mysql-8.0.19-deps.sh
+./mysql-8.0.19-install.sh
+./mysql-8.0.19-init.sh
+./mysql-8.0.19-start.sh &
+cd ..
+
+cd php
+./php-7.4.4-deps.sh
+./php-7.4.4-install.sh
+./php-7.4.4-start.sh
+cd ..
+
+cd nginx
+./nginx-1.17.9-deps.sh
+./nginx-1.17.9-install.sh
+./nginx-1.17.9-start.sh
+cd ..
+
+./post-install.sh
