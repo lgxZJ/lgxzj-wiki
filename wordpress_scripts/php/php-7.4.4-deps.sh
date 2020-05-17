dcb8c1d70877c0796aa690fa1671194bf7d03e72
root <root@li687-97.members.linode.com>
2020-05-17 11:56:56 +0000

    + using php templates for conf

--- wordpress_scripts/php/php-7.4.4-deps.sh
+++ wordpress_scripts/php/php-7.4.4-deps.sh
@@ -16,6 +16,10 @@ iniFileLoc=../lgxzj.ini
 php_install_dir=$(read_ini ${iniFileLoc} install_php_dir)
 download_tmp_dir=$(read_ini ${iniFileLoc} download_leaf_dir_name)
 
+deps_download_url_libc_client=$(read_ini ${iniFileLoc} deps_download_url_libc_client)
+deps_download_name_libc_client=$(read_ini ${iniFileLoc} deps_download_name_libc_client)
+deps_download_prefix_libc_client=$(read_ini ${iniFileLoc} deps_download_prefix_libc_client)
+
 deps_download_url_uw_imap_devel=$(read_ini ${iniFileLoc} deps_download_url_uw_imap_devel)
 deps_download_name_uw_imap_devel=$(read_ini ${iniFileLoc} deps_download_name_uw_imap_devel)
 deps_download_prefix_uw_imap_devel=$(read_ini ${iniFileLoc} deps_download_prefix_uw_imap_devel)
@@ -36,6 +40,14 @@ yum install -y libxml2-devel sqlite-devel krb5-devel openssl-devel bzip2-devel l
 rm -rf ${download_tmp_dir}
 mkdir ${download_tmp_dir}
 cd ${download_tmp_dir}
+
+libc_client_installed=`rpm -qa | grep "${deps_download_prefix_libc_client}"`
+if  [[ -z "${libc_client_installed}" ]]; then
+    wget ${deps_download_url_libc_client}
+    rpm -Uvh ${deps_download_name_libc_client}
+else
+    echo "libc_client installed!"
+fi
 
 imap_installed=`rpm -qa | grep "${deps_download_prefix_uw_imap_devel}"`
 if [[ -z "$imap_installed" ]]; then
--- wordpress_scripts/php/php-7.4.4-start.sh
+++ wordpress_scripts/php/php-7.4.4-start.sh
@@ -9,16 +9,37 @@ if [ ! -f "../ini_reader.sh" ]; then
 fi
 . ../ini_reader.sh
 
+if [ ! -f "../str_replacer.sh" ]; then
+    echo "!!!!!!!!!!!!!!!!!!!!!!!"
+    echo "str_replacer.sh not found"
+    echo "!!!!!!!!!!!!!!!!!!!!!!!"
+    exit 255
+fi
+. ../str_replacer.sh
+
 ##############################################################
 # Load variables from the conf file
 ##############################################################
 iniFileLoc=../lgxzj.ini
 php_install_dir=$(read_ini ${iniFileLoc} install_php_dir)
+php_user=$(read_ini ${iniFileLoc} php_user)
+php_group=$(read_ini ${iniFileLoc} php_group)
+
+##############################################################
+# Replace template variables with conf values
+##############################################################
+rm -f ./php-fpm.conf
+cp ./php-fpm.conf.template ./php-fpm.conf
+replace_str ./php-fpm.conf '${install_php_dir}' ${php_install_dir}
+
+rm -f ./wordpress.conf
+cp ./wordpress.conf.template ./wordpress.conf
+replace_str ./wordpress.conf '${php_user}' ${php_user}
+replace_str ./wordpress.conf '${php_group}' ${php_group}
 
 ##############################################################
 # Start php-fpm
 ##############################################################
-mv ${php_install_dir}/etc/php-fpm.conf.default ${php_install_dir}/etc/php-fpm.conf
 rm -f ${php_install_dir}/etc/php-fpm.d/www.conf.default
 cp ./php-fpm.conf ${php_install_dir}/etc
 cp ./wordpress.conf ${php_install_dir}/etc/php-fpm.d/
