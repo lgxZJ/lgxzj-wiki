1d2e8e5a835bca4b4728728367b825a5ad058819
root <root@li687-97.members.linode.com>
2020-05-17 15:23:38 +0000

    + generate nginx conf using template

--- wordpress_scripts/nginx/nginx-1.17.9-start.sh
+++ wordpress_scripts/nginx/nginx-1.17.9-start.sh
@@ -9,11 +9,28 @@ if [ ! -f "../ini_reader.sh" ]; then
 fi
 . ../ini_reader.sh
 
+if [ ! -f "../str_replacer.sh" ]; then
+    echo "!!!!!!!!!!!!!!!!!!!!!!!"
+    echo "str_replacer.sh not found"
+    echo "!!!!!!!!!!!!!!!!!!!!!!!"
+    exit 255
+fi
+. ../str_replacer.sh
+
 ##############################################################
 # Load variables from the conf file
 ##############################################################
 iniFileLoc=../lgxzj.ini
 nginx_install_dir=$(read_ini ${iniFileLoc} install_nginx_dir)
+blog_domain=$(read_ini ${iniFileLoc} blog_domain)
+
+##############################################################
+# Generate conf file using templates
+##############################################################
+rm -f ./nginx.conf
+cp ./nginx.conf.template ./nginx.conf
+replace_str ./nginx.conf '${blog_domain}' ${blog_domain}
+replace_str ./nginx.conf '${install_nginx_dir}' ${nginx_install_dir}
 
 ##############################################################
 # Start nginx
