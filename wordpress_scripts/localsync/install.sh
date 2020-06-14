2fff9045d81bc49107b17212df392517508444e7
lgxZJ <lgxZJ@outlook.com>
2020-06-14 22:47:55 +0800

    + fix home dir, fix date/sed compatibility on diff os

--- wordpress_scripts/localsync/install.sh
+++ wordpress_scripts/localsync/install.sh
@@ -23,13 +23,20 @@ fi
 iniFileLoc=../lgxzj.ini
 install_backups_dir=$(read_ini ${iniFileLoc} install_backups_dir)
 blog_domain=$(read_ini ${iniFileLoc} blog_domain)
-sshkey_loc=$({read_ini ${iniFileLoc} sshkey_loc})
-sshkey_pub_loc=$({read_ini ${iniFileLoc} sshkey_pub_loc)
-backup_user=$({read_ini ${iniFileLoc} backup_user)
-		 
+sshkey_loc=$(read_ini ${iniFileLoc} sshkey_loc)
+sshkey_pub_loc=$(read_ini ${iniFileLoc} sshkey_pub_loc)
+backup_user=$(read_ini ${iniFileLoc} backup_user)
+
+home_dir=`echo $HOME`
+sshkey_loc="${home_dir}/${sshkey_loc}"
+sshkey_pub_loc="${home_dir}/${sshkey_pub_loc}"
+
 #####################################
 # Setup non-password ssh login
 #####################################
+home_dir=`echo $HOME`
+rm -f ${sshkey_loc}
+rm -f ${sshkey_pub_loc}
 ssh-keygen -t rsa -N "" -f ${sshkey_loc}
 ssh-copy-id -i ${sshkey_pub_loc} ${backup_user}@${blog_domain}
 ssh-add
@@ -41,6 +48,7 @@ rm -f backup_sync.sh
 cp backup_sync.template backup_sync.sh
 replace_str backup_sync.sh '${install_backups_dir}' ${install_backups_dir}
 replace_str backup_sync.sh '${blog_domain}' ${blog_domain}
+replace_str backup_sync.sh '${backup_user}' ${backup_user}
 chmod u+x backup_sync.sh
 
 chmod u+x backup_crontab.sh
