232d91c22ab823d7b8f7c6d93d9607c83beac9b4
root <root@li1442-164.members.linode.com>
2020-06-14 01:19:43 +0000

    + finish restore.sh

--- wordpress_scripts/backups/restore.sh
+++ wordpress_scripts/backups/restore.sh
@@ -14,20 +14,37 @@ fi
 ###############################################
 iniFileLoc=../lgxzj.ini
 mysql_install_dir=$(read_ini ${iniFileLoc} mysql_install_dir)
+install_backups_dir=$(read_ini ${iniFileLoc} install_backups_dir)
 mysql_socket_file_loc=$(read_ini ${iniFileLoc} mysql_socket_file_loc)
 mysql_root_local_password=$(read_ini ${iniFileLoc} mysql_root_local_password)
+wordpress_deploy_dir=$(read_ini ${iniFileLoc} wordpress_deploy_dir)
 
 ##############################################################
 #
 ##############################################################
 backup_sql_file=$1
-if [ "$#" -ne 1 ]; then
+backup_wordpress_file=$2
+if [ "$#" -ne 2 ]; then
     echo ""
-    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-    echo "Usage: restore.sh backup_file.sql"
-    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    echo "Usage: restore.sh backup_file.sql wordpress.tar.gz"
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
     echo ""
     exit 255
 fi
 
+# restore mysql dbs
 ${mysql_install_dir}/bin/mysql --socket ${mysql_socket_file_loc} -u root -p${mysql_root_local_password} < ${backup_sql_file}
+
+# restore wordpress files
+rm -rf backups_tmp
+mkdir backups_tmp
+cd backups_tmp
+tar -zxvf ${backup_wordpress_file}
+
+mkdir -p ${wordpress_deploy_dir}
+source_dir=${wordpress_deploy_dir:1}
+cp -rf ${source_dir}/* ${wordpress_deploy_dir}
+
+cd ..
+rm -rf backups_tmp
