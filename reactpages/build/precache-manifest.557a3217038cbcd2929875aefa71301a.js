f00bcfdc908fba49530b1ee8c9b79140993a9048
lgxZJ <lgxZJ@outlook.com>
2020-07-04 01:00:54 +0800

    + upload build pkg

---
+++ reactpages/build/precache-manifest.557a3217038cbcd2929875aefa71301a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae4a3c40bada38fefadf87fe5652c5a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c97a2e17e429074985c",
+    "url": "/static/css/2.3cb76441.chunk.css"
+  },
+  {
+    "revision": "89543ec8502a8f88949c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4c97a2e17e429074985c",
+    "url": "/static/js/2.1d9edb1a.chunk.js"
+  },
+  {
+    "revision": "e4af76ce3bbbb03f2874740a801d62b0",
+    "url": "/static/js/2.1d9edb1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89543ec8502a8f88949c",
+    "url": "/static/js/main.347787ba.chunk.js"
+  },
+  {
+    "revision": "0a5ed7b2319e7d843b71",
+    "url": "/static/js/runtime-main.bce93c4d.js"
+  }
+]);
